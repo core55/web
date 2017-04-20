b5af0f28e9e68a85f232ea9c50ad629617d757cc
Marcel Eschmann <marcel.eschmann@gmail.com>
2017-04-20 15:41:42 +0200

    Add simple map component

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+  export default {
+    name: 'google-map',
+    data () {
+      return {
+        map: null
+      }
+    },
+    methods: {
+      initMap () {
+        let app = this;
+        app.map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: {lat: -25.363, lng: 131.044}
+        });
+      }
+    },
+    mounted () {
+      window.initMap = this.initMap;
+    },
+  }
+</script>
+
+<style scoped>
+  #map {
+    width: 100%;
+    height: 500px;
+  }
+</style>
