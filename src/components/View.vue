18480aa6a5044634506e42a3aa0a5f93e8c6d218
Qi Li <qi5@kth.se>
2017-04-21 17:21:58 +0200

    add share button

--- src/components/View.vue
+++ src/components/View.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <google-map :callback="initMap" v-loading.fullscreen.lock="loading"></google-map>
-    <el-button size="medium" id="sharebtn" icon="share" @click="createpEW"></el-button>
+    <el-button size="medium" id="sharebtn" icon="share" @click="creatshare"></el-button>
   </div>
 </template>
 
@@ -18,12 +18,33 @@
     data () {
       return {
         loading: true,
-        meetupId: null
-      }
-    },
+        meetupId: null,
+        map: null,
+        pos: null,
+        }
+      },
     methods: {
-        initMap () {
-        },
+      initMap(){
+        let app = this;
+        app.pos = {
+          lat:-34.397 ,
+          lng:150.644
+        };
+        app.map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: app.pos,
+          disableDefaultUI: true
+        });
+
+        app.loading = false;
+        app.map.setCenter(app.pos);
+        this.$message('Map Created.');
+
+      },
+      creatshare(){
+
+
+      },
         async getMeetup() {
           this.meetupId = this.$route.params.id;
           let meetup = await Api.getMeetup(this.meetupId);
@@ -42,7 +63,15 @@
     z-index: 1;
     position: absolute;
     right: 24px;
-    bottom: 690px;
+    bottom: 24px;
+    width: 70px;
+    height: 70px;
+    padding: 10px 16px;
+    font-size: 24px;
+    line-height: 1.33;
+    border-radius: 35px;
   }
+
+
 
 </style>
