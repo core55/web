3826fccedad8014498b8ee93fa0c6dba3a1ac1e0
Marcel Eschmann <marcel.eschmann@gmail.com>
2017-04-26 09:58:46 +0200

    Add base domain to dotenv

--- config/dotenv.example.js
+++ config/dotenv.example.js
@@ -1,4 +1,5 @@
 module.exports = {
+  APP_DOMAIN: '"http://localhost:8080/#"',
   GOOGLE_MAP_KEY: '"TOKEN_HERE"',
   GOOGLE_MAP_URL: '"https://maps.googleapis.com/maps/api/js"',
   GOOGLE_GEOLOCATION_URL: '"https://www.googleapis.com/geolocation/v1/geolocate?key="',
