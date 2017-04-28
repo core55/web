bd7547e6905bc25eac5661f153a19efe89e0f5b2
Marcel Eschmann <marcel.eschmann@gmail.com>
2017-04-28 10:35:02 +0200

    Fix initials function

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 
 export default class Helper {
-  static getInitials(nickname){
+  static getInitials(nickname) {
     if (!nickname) { return '?'; }
 
     var trimmed = nickname.trim().split(' ');
     for (var i = 0; i < trimmed.length; i++) {
       trimmed[i] = trimmed[i].charAt(0);
     }
+
+    return trimmed.join('');
   }
 }
