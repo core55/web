e22958d957bb16c2f7532f821383683129676ee9
Dean Rauschenbusch <deanr@live.de>
2017-05-07 22:07:19 +0200

    Implement Helper function to find distance from A to B

--- src/helper/index.js
+++ src/helper/index.js
@@ -123,5 +123,24 @@ export default class Helper {
     var currentTime = (new Date().getTime() / 1000).toFixed(0);
 
     return ((currentTime - userUpdatedAt)/60).toFixed(1);
-  }  
+  }
+
+  // Distance Between two Coordinates
+  // Input: 2 langnitudes and 2 longitudes 
+  // Output: distance between in meters
+  static distanceFromAtoB(lat1, lon1, lat2, lon2){
+    var R = 6371e3; // metres
+    var φ1 = lat1 * Math.PI / 180;  
+    var φ2 = lat2 * Math.PI / 180;
+    var Δφ = (lat2-lat1) * Math.PI / 180;
+    var Δλ = (lon2-lon1) * Math.PI / 180;
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var distance = R * c;
+
+    return distance;
+  }
 }
