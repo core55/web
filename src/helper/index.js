599f2cc1a5d2ee72c647adddafb73d6d4d1cdefd
Dean Rauschenbusch <deanr@live.de>
2017-05-08 07:55:16 +0200

    Refactor Code

--- src/helper/index.js
+++ src/helper/index.js
@@ -130,17 +130,16 @@ export default class Helper {
   // Output: distance between in meters
   static distanceFromAtoB(lat1, lon1, lat2, lon2){
     var R = 6371e3; // metres
-    var φ1 = lat1 * Math.PI / 180;  
-    var φ2 = lat2 * Math.PI / 180;
-    var Δφ = (lat2-lat1) * Math.PI / 180;
-    var Δλ = (lon2-lon1) * Math.PI / 180;
+    var x1 = lat1 * Math.PI / 180;  
+    var x2 = lat2 * Math.PI / 180;
+    var xDelta = (lat2-lat1) * Math.PI / 180;
+    var yDelta = (lon2-lon1) * Math.PI / 180;
 
-    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var a = Math.sin(xDelta/2) * Math.sin(xDelta/2) +
+            Math.cos(x1) * Math.cos(x2) *
+            Math.sin(yDelta/2) * Math.sin(yDelta/2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var distance = R * c;
 
-    return distance;
+    return R * c;
   }
 }
