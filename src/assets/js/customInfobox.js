6fb6454f0ecf37149b75bf309c5fb9857c85969f
qi5 <qi5@kth.se>
2017-05-09 16:05:19 +0200

    indentation fix

--- src/assets/js/customInfobox.js
+++ src/assets/js/customInfobox.js
@@ -45,7 +45,6 @@ export default class customInfobox extends google.maps.OverlayView {
     var u = document.createTextNode(this.username_);
     this.content2= document.createElement('p');
     var s =document.createTextNode(this.status_);
-
     //name
     this.content1.style.position = 'absolute';
     this.content1.style.fontSize='15px';
@@ -78,24 +77,15 @@ export default class customInfobox extends google.maps.OverlayView {
   }
 
   draw() {
-    // We use the south-west and north-east
-    // coordinates of the overlay to peg it to the correct position and size.
-    // To do this, we need to retrieve the projection from the overlay.
     var overlayProjection = this.getProjection();
-
-    // Retrieve the south-west and north-east coordinates of this overlay
-    // in LatLngs and convert them to pixel coordinates.
-    // We'll use these coordinates to resize the div.
     var position = this.myLatlng_;
     var px = overlayProjection.fromLatLngToDivPixel(position);
-
-    // Resize the image's div to fit the indicated dimensions.
+//postion
     var div = this.div_;
     div.style.left = px.x+16+ 'px';
     div.style.top = px.y -128+ 'px';
     div.style.width = 150 + 'px';
     div.style.minHeight = 60+ 'px';
-
     this.element=document.getElementById(this.div_.id);
 
   }
@@ -120,7 +110,6 @@ export default class customInfobox extends google.maps.OverlayView {
       this.div_.setAttribute("v-if", "show");
       this.div_.className="bounce-enter-active";
       console.log(this.div_);
-
     }
   }
 //not useful (further expierment required)
@@ -132,83 +121,3 @@ export default class customInfobox extends google.maps.OverlayView {
   };
 
 }
-
-
-
-/*
----------------------------------------------------------------------------------------------------------------
-*/
-
-/*
-
-export default class customInfobox extends google.maps.OverlayView {
-  constructor(bounds, image, map) {
-    super();
-    // Initialize all properties.
-    this.bounds_ = bounds;
-    this.image_ = image;
-    this.map_ = map;
-
-    // Define a property to hold the image's div. We'll
-    // actually create this div upon receipt of the onAdd()
-    // method so we'll leave it null for now.
-    this.div_ = null;
-
-    // Explicitly call setMap on this overlay.
-    this.setMap(map);
-  }
-
-  onAdd() {
-    var div = document.createElement('div');
-    div.style.borderStyle = 'none';
-    div.style.borderWidth = '0px';
-    div.style.position = 'absolute';
-
-    // Create the img element and attach it to the div.
-    var img = document.createElement('img');
-    img.src = this.image_;
-    img.style.width = '100%';
-    img.style.height = '100%';
-    img.style.position = 'absolute';
-    div.appendChild(img);
-
-    this.div_ = div;
-
-    // Add the element to the "overlayLayer" pane.
-    var panes = this.getPanes();
-    panes.overlayLayer.appendChild(div);
-  }
-
-  draw() {
-    // We use the south-west and north-east
-    // coordinates of the overlay to peg it to the correct position and size.
-    // To do this, we need to retrieve the projection from the overlay.
-    var overlayProjection = this.getProjection();
-
-    // Retrieve the south-west and north-east coordinates of this overlay
-    // in LatLngs and convert them to pixel coordinates.
-    // We'll use these coordinates to resize the div.
-    var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-    var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-    // Resize the image's div to fit the indicated dimensions.
-    var div = this.div_;
-    div.style.left = sw.x + 'px';
-    div.style.top = ne.y + 'px';
-    div.style.width = (ne.x - sw.x) + 'px';
-    div.style.height = (sw.y - ne.y) + 'px';
-  }
-
-  onRemove() {
-    this.div_.parentNode.removeChild(this.div_);
-    this.div_ = null;
-  }
-
-show() {
-  if (div_) {
-    div_.style.visibility = 'visible';
-  }
-}
-
-}
-*/
