788e2b59f2b6834d40e2fc077573de2af43d6b18
Dean Rauschenbusch <deanr@live.de>
2017-05-13 17:54:22 +0200

    Import Menu Icons

---
+++ src/components/MenuOption.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="drawer-list-element">
+      <div class="drawer-icon" id="el-1">
+      </div>
+      <h1 class="list-title">Profile</h1>
+  </div>
+</template>
+
+
+
+
+
+
