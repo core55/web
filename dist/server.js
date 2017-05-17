24ff944fd2d67c89674ec3ba6660f0bc5b5abea7
Marcel Eschmann <marcel.eschmann@gmail.com>
2017-05-17 18:08:45 +0200

    Enforce https

--- dist/server.js
+++ dist/server.js
@@ -3,19 +3,11 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
+
+var enforce = require('express-sslify');
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 app.use(serveStatic(__dirname));
-
-var enforceHttps = function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] != 'https' || !req.secure) {
-    var url = ['https://', req.hostname, req.url].join('');
-    return res.redirect(url);
-  }
-
-  next();
-}
-
-// enable ssl redirect
-app.use(enforceHttps);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
