3fbd9cf3224b5f86fbf3aae70a1343f0020270e4
Dean Rauschenbusch <deanr@live.de>
2017-05-17 22:59:11 +0200

    Store Key:shareLocation with Value:true in localStorage

--- src/components/View.vue
+++ src/components/View.vue
@@ -275,6 +275,7 @@ export default {
      *  Join a meetup if not already in. Use local storage to persist user data
      */
     async joinEvent() {
+      UserHelper.writeToLocalStorage('shareLocation', true);  //share location!
       let user = UserHelper.getUser();
       let userMeetups = UserHelper.getUserMeetups();
       if (!userMeetups) { userMeetups = []; }
@@ -451,7 +452,7 @@ export default {
           google.maps.event.addDomListener(marker.getDiv(), 'click', function () {
             var userMarkerInformation = app.markersMap[indexOfMarker];
             var marker = userMarkerInformation.marker;
-            
+
             app.checkDirection(marker);
 
             // close info window if one is already open
